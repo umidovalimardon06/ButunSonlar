7382a1caa6ae440f7f762f68439efd980592f2e9
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-06 01:31:01 +0500

    add Integer04

---
+++ Integer04/Main/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+// Umidov Alimardon //
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A,B;
+
+        while (true) {
+
+            System.out.print("A:");
+            A = Math.abs(in.nextInt());
+            System.out.print("B:");
+            B = Math.abs(in.nextInt());
+
+            if (A > B) {
+                break;
+            }
+            System.out.println("A>B bo'lishi kerak!");
+        }
+
+
+        System.out.println("B,A kesmada "+(A/B)+" marta joylasha oladi.");
+
+
+
+    }
+}
