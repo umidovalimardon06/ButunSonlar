c61dbbce63447d7a9693db61a14f743eecdb3391
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-06 00:52:30 +0500

    add Integer1

---
+++ Integer1/Main/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+// Umidov Alimardon
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int L ;
+        System.out.print("L:");
+        L = Math.abs(in.nextInt());
+
+        System.out.println("Natija: "+(L/100));
+
+
+
+    }
+}
