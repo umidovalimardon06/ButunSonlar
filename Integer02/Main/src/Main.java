b3bba508773b78ab80a14be159723ecdea6a79d3
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-06 01:02:57 +0500

    add Intager2

---
+++ Integer02/Main/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+// Umidov Alimardon //
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int M ;
+        System.out.print("Kg da massa kiritign:");
+        M = Math.abs(in.nextInt());
+
+        System.out.println("Natija: "+(M/1000));
+
+
+
+
+    }
+}
