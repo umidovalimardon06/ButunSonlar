d3d45cc48baca24e340ca7872a173725e25dd8c8
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-08 20:23:54 +0500

    add Integer23

---
+++ Integer23/Main/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+// Umidov Alimardon.
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int sekund ;
+
+        while (true) {
+
+            System.out.print("Kun boshidan boshlab o'tgan sekund:");
+            sekund = in.nextInt();
+
+            if (sekund>=0) {
+                break;
+            }
+            System.out.println("manfiy bo'lishi mumkun emas!");
+        }
+
+        System.out.println("Kun boshidan "+(sekund/3600) +" soat, "+(sekund%3600/60)+" minut, " + (sekund%60) + " sekund o'tdi.");
+
+    }
+}
